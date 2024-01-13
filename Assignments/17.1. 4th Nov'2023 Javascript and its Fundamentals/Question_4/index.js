60d25d9db2a46467e78c73c5cb052e9107160bf7
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-13 22:56:00 +0530

    complete question 4 of assignment 17.1

---
+++ Assignments/17.1. 4th Nov'2023 Javascript and its Fundamentals/Question_4/index.js
@@ -0,0 +1,9 @@
+// Examples:
+let variableName; // Declaration without assignment
+
+let age = 25; // Declaration and assignment
+
+let firstName; // Declaration
+firstName = "John"; // Assignment
+
+console.log("Hello, " + firstName + "!"); // Output: Hello, John!
