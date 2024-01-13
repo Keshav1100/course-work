9294c1cf999960847cc3425668ff8043e82e8983
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-13 23:16:19 +0530

    complete question 6 of assignment 17.1

---
+++ Assignments/17.1. 4th Nov'2023 Javascript and its Fundamentals/Question_6/index.js
@@ -0,0 +1,23 @@
+// Examples:
+
+let a = 10;
+let b = 5;
+
+// Calculate the sum of a and b
+let c = a + b;
+
+// Display the result
+console.log(c);
+// Output: 15
+
+let firstNumber = 10;
+let secondNumber = 5;
+
+// Calculate the sum of the two numbers
+let sumResult = firstNumber + secondNumber;
+
+// Display the result
+console.log(sumResult);
+// Output: 15
+
+
