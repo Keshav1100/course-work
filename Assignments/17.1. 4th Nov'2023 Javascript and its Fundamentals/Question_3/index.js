63443401d9f5926077e02ccf648727933afffd07
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-13 22:55:37 +0530

    complete question 3 of assignment 17.1

---
+++ Assignments/17.1. 4th Nov'2023 Javascript and its Fundamentals/Question_3/index.js
@@ -0,0 +1,20 @@
+// Examples:
+
+// 1. Datatype Number:-
+let count = 10; // integer
+let price = 29.99; // floating-point number
+
+// 2. Datatype String:-
+let name = "John";
+let message = "Hello, World!";
+
+// 3. Datatype Boolean:-
+let isReady = true;
+let isWorking = false;
+
+// 4. Datatype Underfined:-
+let x;
+console.log(x); // Output: undefined
+
+// 5. Datatype null:-
+let car = null;
