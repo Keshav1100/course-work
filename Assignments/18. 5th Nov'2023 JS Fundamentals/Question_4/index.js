36c8c6a3259f7c95dc6e77799a213f5c5f764725
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-14 19:51:06 +0530

    complete ques 4 of assignment 18

---
+++ Assignments/18. 5th Nov'2023 JS Fundamentals/Question_4/index.js
@@ -0,0 +1,9 @@
+// Example age
+let age = 20;
+
+// Using a ternary operator to check voting eligibility
+let eligibility = age < 18 ? "You cannot vote" : "You can vote";
+
+// Displaying the result
+console.log("Age: " + age);
+console.log(eligibility);
