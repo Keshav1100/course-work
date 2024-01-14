0ade9835289cc8088d559e30d40ee2d2b5959b56
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-14 19:48:15 +0530

    complete ques 3 of assignment 18

---
+++ Assignments/18. 5th Nov'2023 JS Fundamentals/Question_3/index.js
@@ -0,0 +1,11 @@
+// Example number
+let number = -7;
+
+// Using nested ternary operator to check the number
+let result = number > 0 ? "Positive" : (number < 0 ? "Negative" : "Zero");
+
+// Displaying the result
+console.log("Number: " + number);
+console.log("Result: " + result);
+
+
