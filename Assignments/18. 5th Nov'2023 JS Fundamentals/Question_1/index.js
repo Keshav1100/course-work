433de54cf6bfc413ef19548ebac747e2c19b1b59
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-14 19:43:59 +0530

    complete ques 1 of assignment 18

---
+++ Assignments/18. 5th Nov'2023 JS Fundamentals/Question_1/index.js
@@ -0,0 +1,12 @@
+// Syntax
+//condition ? expressionIfTrue : expressionIfFalse;
+
+// Example value
+let number = 7;
+
+// Checking if the number is even or odd using the ternary operator
+let result = number % 2 === 0 ? "Even" : "Odd";
+
+// Displaying the result
+console.log("Number: " + number);
+console.log("Result: " + result);
