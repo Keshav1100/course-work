fdce06af85137278dd370d5cf1d4e2e5dd0e6249
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-14 19:25:17 +0530

    complete ques 6 of assignment 17.2

---
+++ Assignments/17. 4th Nov and 5th Nov Js/17.2. 4th Nov'2023 Javascript Operators/Question_6/index.js
@@ -0,0 +1,11 @@
+// Example values
+let weightInKg = 70; // Weight in kilograms
+let heightInMeters = 1.75; // Height in meters
+
+// Calculating BMI
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+
+// Displaying the result
+console.log("Weight: " + weightInKg + " kg");
+console.log("Height: " + heightInMeters + " meters");
+console.log("BMI: " + bmi.toFixed(2));
