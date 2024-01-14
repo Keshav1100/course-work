882dab78361c7cbbc4b0343f05ab70a75ef8b4de
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-14 18:58:36 +0530

    complete ques 3 of assignment 17.2

---
+++ Assignments/17. 4th Nov and 5th Nov Js/17.2. 4th Nov'2023 Javascript Operators/Question_3/index.js
@@ -0,0 +1,16 @@
+// Examples
+
+let x = 5;
+let y = -x; // Unary minus, changes the sign of x
+let isTrue = true;
+let isFalse = !isTrue; // Unary NOT, negates a Boolean value
+
+let a = 5;
+let b = 2;
+let sum = a + b; // Addition, binary operator
+let isGreaterThan = a > b; // Greater than, binary operator
+
+condition ? expressionIfTrue : expressionIfFalse;
+
+let age = 20;
+let isAdult = age >= 18 ? "Yes" : "No";
