9648e48950c98083f729f53272aa40063f85ae33
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-14 18:45:48 +0530

    complete ques 2 of assignment 17.2

---
+++ Assignments/17. 4th Nov and 5th Nov Js/17.2. 4th Nov'2023 Javascript Operators/Question_2/index.js
@@ -0,0 +1,31 @@
+// Examples
+
+// Arithematic Operators
+let a = 5;
+let b = 2;
+let sum = a + b; // Addition
+let difference = a - b; // Subtraction
+let product = a * b; // Multiplication
+let quotient = a / b; // Division
+
+// Comparision Operators
+let x = 10;
+let y = 5;
+let isEqual = x === y; // Strict equality
+let isGreaterThan = x > y; // Greater than
+
+// Logical Operators
+let isTrue = true;
+let isFalse = false;
+let logicalAnd = isTrue && isFalse; // Logical AND
+let logicalOr = isTrue || isFalse; // Logical OR
+let logicalNot = !isTrue; // Logical NOT
+
+// Assignment Operators
+let a2 = 10;
+a2 += 5; // Equivalent to a = a + 5
+
+// Unary Operators
+let count = 0;
+count++; // Increment by 1
+count--; // Decrement by 1
