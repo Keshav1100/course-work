4ba7f9360509525db3e972e231fd917c0d104fd2
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-14 19:25:31 +0530

    complete ques 7 of assignment 17.2

---
+++ Assignments/17. 4th Nov and 5th Nov Js/17.2. 4th Nov'2023 Javascript Operators/Question_7/index.js
@@ -0,0 +1,9 @@
+// Example value
+let radius = 10; // Radius of the circle
+
+// Calculating the area of the circle
+let area = Math.PI * Math.pow(radius, 2);
+
+// Displaying the result
+console.log("Radius: " + radius);
+console.log("Area of the Circle: " + area.toFixed(2));
