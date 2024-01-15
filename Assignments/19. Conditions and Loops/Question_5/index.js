f3f15e21e49221437c0ca39b0afae7f550630ea0
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-15 19:33:21 +0530

    complete ques 5 of assignment 19

---
+++ Assignments/19. Conditions and Loops/Question_5/index.js
@@ -0,0 +1,18 @@
+
+
+// Ascending order
+console.log("Ascending Order:");
+let ascendingCounter = 1;
+while (ascendingCounter <= 25) {
+  console.log(ascendingCounter);
+  ascendingCounter++;
+}
+
+// Descending order
+console.log("Descending Order:");
+let descendingCounter = 25;
+while (descendingCounter >= 1) {
+  console.log(descendingCounter);
+  descendingCounter--;
+}
+
