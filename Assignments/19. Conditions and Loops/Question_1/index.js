7840801f76c7ec4a4b3538659103a742d51b2bb0
Keshav Agarwal <keshav1105ag@gmail.com>
2024-01-15 17:12:19 +0530

    complete ques 1 of assignment 19

---
+++ Assignments/19. Conditions and Loops/Question_1/index.js
@@ -0,0 +1,57 @@
+// Example age
+let condition;
+let condition1;
+let condition2;
+
+// If statement examples
+
+if (condition) {
+  // Code to be executed if the condition is true
+}
+
+let age = 20;
+
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You cannot vote yet.");
+}
+// Output: You are eligible to vote.
+
+// If-else statement examples
+
+if (condition) {
+  // Code to be executed if the condition is true
+} else {
+  // Code to be executed if the condition is false
+}
+
+let number = 7;
+
+if (number % 2 === 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
+//Output: The number is odd.
+
+// If-else-If-else statement examples
+
+if (condition1) {
+  // Code to be executed if condition1 is true
+} else if (condition2) {
+  // Code to be executed if condition2 is true
+} else {
+  // Code to be executed if none of the conditions are true
+}
+
+let score = 85;
+
+if (score >= 90) {
+  console.log("Excellent!");
+} else if (score >= 70) {
+  console.log("Good job!");
+} else {
+  console.log("You need improvement.");
+}
+//Output: Good job!
